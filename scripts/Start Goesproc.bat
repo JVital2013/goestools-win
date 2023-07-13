354b2b4af337fdcba887d7e98e499f2b7601398a
Jamie Vital <JVital2013@gmail.com>
2023-07-13 16:44:19 -0400

    Fix IP

--- scripts/Start Goesproc.bat
+++ scripts/Start Goesproc.bat
@@ -1,4 +1,4 @@
 @echo off
 title goesproc
-bin\goesproc.exe --config config/goesproc-goesr.conf --subscribe tcp://192.168.1.137:5004
+bin\goesproc.exe --config config/goesproc-goesr.conf --subscribe tcp://127.0.0.1:5004
 pause
