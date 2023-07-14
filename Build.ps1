3ee5e37377112b7d92b761bbbd3b621b73cb5429
Jamie Vital <jvital2013@gmail.com>
2023-07-14 14:42:06 -0400

    Switch to rebuild libairspy and enable 32-Bit Builds

--- Build.ps1
+++ Build.ps1
@@ -17,42 +17,53 @@ Remove-Item build -Force -Recurse -ErrorAction SilentlyContinue | Out-null
 mkdir build | Out-null
 cd build | Out-null
 
-#Do the build
-cmake .. "-DCMAKE_TOOLCHAIN_FILE=$($vcpkg.Replace("\", "/"))/scripts/buildsystems/vcpkg.cmake" -G "Visual Studio 17 2022"
-cmake --build . --config Release
+#Do the builds
+foreach($arch in $("win32", "x64"))
+{
+    Write-Output "Building for $arch..."
+    mkdir "build-$arch" | Out-Null
+    cd "build-$arch"
 
-#Create the bundle folder and put everything in it
-Write-Output "Making distributable folder..."
-mkdir dist | Out-Null
-cd dist
-mkdir bin  | Out-Null
-mkdir Saved | Out-Null
+    cmake ..\.. "-DCMAKE_TOOLCHAIN_FILE=$($vcpkg.Replace("\", "/"))/scripts/buildsystems/vcpkg.cmake" -G "Visual Studio 17 2022" -A $arch
+    cmake --build . --config Release
 
-cp -Force ..\..\build\src\assembler\Release\*.exe bin/
-cp -Force ..\..\build\src\dcs\Release\*.exe bin/
-cp -Force ..\..\build\src\dcs\Release\*.dll bin/
-cp -Force ..\..\build\src\decoder\Release\*.exe bin/
-cp -Force ..\..\build\src\goesemwin\Release\*.exe bin/
-cp -Force ..\..\build\src\goesemwin\Release\*.dll bin/
-cp -Force ..\..\build\src\goeslrit\Release\*.exe bin/
-cp -Force ..\..\build\src\goeslrit\Release\*.dll bin/
-cp -Force ..\..\build\src\goespackets\Release\*.exe bin/
-cp -Force ..\..\build\src\goespackets\Release\*.dll bin/
-cp -Force ..\..\build\src\goesproc\Release\*.exe bin/
-cp -Force ..\..\build\src\goesproc\Release\*.dll bin/
-cp -Force ..\..\build\src\goesrecv\Release\*.exe bin/
-cp -Force ..\..\build\src\goesrecv\Release\*.dll bin/
-cp -Force ..\..\build\src\lib\Release\*.exe bin/
-cp -Force ..\..\build\src\lib\Release\*.dll bin/
-cp -Force ..\..\build\src\lrit\Release\*.exe bin/
-cp -Force ..\..\build\src\lrit\Release\*.dll bin/
+    #Create the bundle folder and put everything in it
+    Write-Output "Making $arch distributable folder..."
+    cd ..
+    mkdir "dist-$arch" | Out-Null
+    cd "dist-$arch"
 
-cp ..\..\README.md .
-cp $vcpkg\installed\x64-windows\share\proj\proj.db .
-cp -r ..\..\share .
-cp -r ..\..\LICENSES .
-cp -r ..\..\config .
-cp ..\..\scripts\* .
+    mkdir bin  | Out-Null
+    mkdir Saved | Out-Null
+
+    cp -Force ..\build-$arch\src\assembler\Release\*.exe bin/
+    cp -Force ..\build-$arch\src\dcs\Release\*.exe bin/
+    cp -Force ..\build-$arch\src\dcs\Release\*.dll bin/
+    cp -Force ..\build-$arch\src\decoder\Release\*.exe bin/
+    cp -Force ..\build-$arch\src\goesemwin\Release\*.exe bin/
+    cp -Force ..\build-$arch\src\goesemwin\Release\*.dll bin/
+    cp -Force ..\build-$arch\src\goeslrit\Release\*.exe bin/
+    cp -Force ..\build-$arch\src\goeslrit\Release\*.dll bin/
+    cp -Force ..\build-$arch\src\goespackets\Release\*.exe bin/
+    cp -Force ..\build-$arch\src\goespackets\Release\*.dll bin/
+    cp -Force ..\build-$arch\src\goesproc\Release\*.exe bin/
+    cp -Force ..\build-$arch\src\goesproc\Release\*.dll bin/
+    cp -Force ..\build-$arch\src\goesrecv\Release\*.exe bin/
+    cp -Force ..\build-$arch\src\goesrecv\Release\*.dll bin/
+    cp -Force ..\build-$arch\src\lib\Release\*.exe bin/
+    cp -Force ..\build-$arch\src\lib\Release\*.dll bin/
+    cp -Force ..\build-$arch\src\lrit\Release\*.exe bin/
+    cp -Force ..\build-$arch\src\lrit\Release\*.dll bin/
+
+    cp ..\..\README.md .
+    cp $vcpkg\installed\x64-windows\share\proj\proj.db .
+    cp -r ..\..\share .
+    cp -r ..\..\LICENSES .
+    cp -r ..\..\config .
+    cp ..\..\scripts\* .
+
+    cd ..
+}
 
 Write-Output "Done! Your build is ready at $($(pwd).path)"
 pause
--- Configure-VCPKG.ps1
+++ Configure-VCPKG.ps1
@@ -17,35 +17,24 @@ git clone https://github.com/microsoft/vcpkg
 cd vcpkg
 git checkout 7771648 #Force a known working version
 .\bootstrap-vcpkg.bat
-.\vcpkg.exe install opencv:x64-windows proj:x64-windows rtlsdr:x64-windows dirent:x64-windows getopt-win32:x64-windows nanomsg:x64-windows nlohmann-json:x64-windows
+.\vcpkg.exe install --triplet x64-windows opencv proj rtlsdr dirent getopt-win32 nanomsg nlohmann-json
+.\vcpkg.exe install --triplet x86-windows opencv proj rtlsdr dirent getopt-win32 nanomsg nlohmann-json
 
 #Install libaec
-cp "$(Split-Path -Parent $MyInvocation.MyCommand.Path)\vendor\libaec\*.dll" "$vcpkg\installed\x64-windows\bin"
-cp "$(Split-Path -Parent $MyInvocation.MyCommand.Path)\vendor\libaec\*.lib" "$vcpkg\installed\x64-windows\lib"
+cp "$(Split-Path -Parent $MyInvocation.MyCommand.Path)\vendor\libaec\x64\*.dll" "$vcpkg\installed\x64-windows\bin"
+cp "$(Split-Path -Parent $MyInvocation.MyCommand.Path)\vendor\libaec\x64\*.lib" "$vcpkg\installed\x64-windows\lib"
+cp "$(Split-Path -Parent $MyInvocation.MyCommand.Path)\vendor\libaec\x86\*.dll" "$vcpkg\installed\x86-windows\bin"
+cp "$(Split-Path -Parent $MyInvocation.MyCommand.Path)\vendor\libaec\x86\*.lib" "$vcpkg\installed\x86-windows\lib"
 cp "$(Split-Path -Parent $MyInvocation.MyCommand.Path)\vendor\libaec\*.h" "$vcpkg\installed\x64-windows\include"
+cp "$(Split-Path -Parent $MyInvocation.MyCommand.Path)\vendor\libaec\*.h" "$vcpkg\installed\x86-windows\include"
 
-#AirSpy Library (thanks @Aang23!)
-cd "$vcpkg\installed\x64-windows\bin"
-curl.exe -LJO https://github.com/Aang23/vcpkg/raw/master/installed/x64-windows/bin/airspy.dll
-curl.exe -LJO https://github.com/Aang23/vcpkg/raw/master/installed/x64-windows/bin/airspyhf.dll
-curl.exe -LJO https://github.com/Aang23/vcpkg/raw/master/installed/x64-windows/bin/pthreadVC2.dll
-cd "$vcpkg\installed\x64-windows\lib"
-curl.exe -LJO https://github.com/Aang23/vcpkg/raw/master/installed/x64-windows/lib/airspy.lib
-curl.exe -LJO https://github.com/Aang23/vcpkg/raw/master/installed/x64-windows/lib/airspyhf.lib
-
-mkdir "$vcpkg\installed\x64-windows\include\libairspy"
-cd "$vcpkg\installed\x64-windows\include\libairspy"
-curl.exe -LJO https://github.com/Aang23/vcpkg/raw/master/installed/x64-windows/include/libairspy/airspy.h
-curl.exe -LJO https://github.com/Aang23/vcpkg/raw/master/installed/x64-windows/include/libairspy/airspy_commands.h
-curl.exe -LJO https://github.com/Aang23/vcpkg/raw/master/installed/x64-windows/include/libairspy/filters.h
-curl.exe -LJO https://github.com/Aang23/vcpkg/raw/master/installed/x64-windows/include/libairspy/iqconvert_float.h
-curl.exe -LJO https://github.com/Aang23/vcpkg/raw/master/installed/x64-windows/include/libairspy/iqconvert_int16.h
-
-mkdir "$vcpkg\installed\x64-windows\include\libairspyhf"
-cd "$vcpkg\installed\x64-windows\include\libairspyhf"
-curl.exe -LJO https://github.com/Aang23/vcpkg/raw/master/installed/x64-windows/include/libairspyhf/airspyhf.h
-curl.exe -LJO https://github.com/Aang23/vcpkg/raw/master/installed/x64-windows/include/libairspyhf/airspyhf_commands.h
-curl.exe -LJO https://github.com/Aang23/vcpkg/raw/master/installed/x64-windows/include/libairspyhf/iqbalancer.h
+#Install libairspy
+cp "$(Split-Path -Parent $MyInvocation.MyCommand.Path)\vendor\libairspy\x64\*.dll" "$vcpkg\installed\x64-windows\bin"
+cp "$(Split-Path -Parent $MyInvocation.MyCommand.Path)\vendor\libairspy\x64\*.lib" "$vcpkg\installed\x64-windows\lib"
+cp "$(Split-Path -Parent $MyInvocation.MyCommand.Path)\vendor\libairspy\x86\*.dll" "$vcpkg\installed\x86-windows\bin"
+cp "$(Split-Path -Parent $MyInvocation.MyCommand.Path)\vendor\libairspy\x86\*.lib" "$vcpkg\installed\x86-windows\lib"
+cp -r "$(Split-Path -Parent $MyInvocation.MyCommand.Path)\vendor\libairspy\libairspy" "$vcpkg\installed\x64-windows\include"
+cp -r "$(Split-Path -Parent $MyInvocation.MyCommand.Path)\vendor\libairspy\libairspy" "$vcpkg\installed\x86-windows\include"
 
 Write-Output "Done!"
 cd "$(Split-Path -Parent $MyInvocation.MyCommand.Path)"
